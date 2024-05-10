a0bdb346b72ebdc19011d77ea2c912f806d900eb
O3H <owen77stubbs@gmail.com>
2024-05-10 21:55:15 +0100

    project-wide cleanup
    project-wide cleanup

--- Patches/TerminalPatch.cs
+++ Patches/TerminalPatch.cs
@@ -1,41 +1,40 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Linq;
+using HarmonyLib;
 using RandomMoons.ConfigUtils;
 using RandomMoons.Utils;
-using System;
-using System.Linq;
 
-namespace RandomMoons.Patches
+namespace RandomMoons.Patches;
+
+/// <summary>
+/// Patches terminal. Learn to read
+/// </summary>
+[HarmonyPatch(typeof(Terminal))]
+public class TerminalPatch
 {
-    /// <summary>
-    /// Patches terminal. Learn to read
-    /// </summary>
-    [HarmonyPatch(typeof(Terminal))]
-    public class TerminalPatch
+    // Checks if a player leaved upon confirming explore command
+    [HarmonyPatch("QuitTerminal")]
+    [HarmonyPrefix]
+    public static void QuitTerminalPatch(Terminal __instance)
     {
-        // Checks if a player leaved upon confirming explore command
-        [HarmonyPatch("QuitTerminal")]
-        [HarmonyPrefix]
-        public static void QuitTerminalPatch(Terminal __instance)
+        if(States.isInteracting)
         {
-            if(States.isInteracting)
-            {
-                States.closedUponConfirmation = true;
-            }
+            States.closedUponConfirmation = true;
         }
+    }
 
-        // Checks if the moon selection config entry can be set to MODDED
-        [HarmonyPatch("BeginUsingTerminal")]
-        [HarmonyPrefix]
-        public static void BeginUsingTerminalPatch(Terminal __instance)
+    // Checks if the moon selection config entry can be set to MODDED
+    [HarmonyPatch("BeginUsingTerminal")]
+    [HarmonyPrefix]
+    public static void BeginUsingTerminalPatch(Terminal __instance)
+    {
+        if(SyncConfig.MoonSelectionType.Value == MoonSelection.MODDED)
         {
-            if(SyncConfig.MoonSelectionType.Value == MoonSelection.MODDED)
-            {
-                foreach(SelectableLevel lvl in __instance.moonsCatalogueList) {
-                    if(!States.vanillaMoons.Contains(lvl.sceneName)) { return; }
-                }
-
-                SyncConfig.MoonSelectionType.Value = MoonSelection.ALL;
+            foreach(SelectableLevel lvl in __instance.moonsCatalogueList) {
+                if(!States.vanillaMoons.Contains(lvl.sceneName)) { return; }
             }
+
+            SyncConfig.MoonSelectionType.Value = MoonSelection.ALL;
         }
     }
 }
